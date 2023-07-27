d065012ef8031d7dddf1cf66bae5bbd44a2fb31f
Greg Bumgardner <gbumgard@gmail.com>
2023-07-27 07:39:49 -0700

    Add LED control.

--- src/main.cpp
+++ src/main.cpp
@@ -1,124 +1,3 @@
-#if 0
-/*************************************************** 
-  This is an example for our Adafruit 16-channel PWM & Servo driver
-  Servo test - this will drive 8 servos, one after the other on the
-  first 8 pins of the PCA9685
-
-  Pick one up today in the adafruit shop!
-  ------> http://www.adafruit.com/products/815
-  
-  These drivers use I2C to communicate, 2 pins are required to  
-  interface.
-
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
-
-#include <Wire.h>
-#include <Adafruit_PWMServoDriver.h>
-
-// called this way, it uses the default address 0x40
-Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-// you can also call it with a different address you want
-//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
-// you can also call it with a different address and I2C interface
-//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire);
-int us2count(int us) {
-  return (us * 4096L) / 20000L;
-}
-
-// Depending on your servo make, the pulse width min and max may vary, you 
-// want these to be as small/large as possible without hitting the hard stop
-// for max range. You'll have to tweak them as necessary to match the servos you
-// have!
-#define SERVOMIN  900 //150 // This is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  2100 // 600 // This is the 'maximum' pulse length count (out of 4096)
-#define USMIN  600 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
-#define USMAX  2400 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
-#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
-
-// our servo # counter
-uint8_t servonum = 0;
-
-void setup() {
-  Serial.begin(9600);
-  Serial.println("8 channel Servo test!");
-
-  pwm.begin();
-  /*
-   * In theory the internal oscillator (clock) is 25MHz but it really isn't
-   * that precise. You can 'calibrate' this by tweaking this number until
-   * you get the PWM update frequency you're expecting!
-   * The int.osc. for the PCA9685 chip is a range between about 23-27MHz and
-   * is used for calculating things like writeMicroseconds()
-   * Analog servos run at ~50 Hz updates, It is importaint to use an
-   * oscilloscope in setting the int.osc frequency for the I2C PCA9685 chip.
-   * 1) Attach the oscilloscope to one of the PWM signal pins and ground on
-   *    the I2C PCA9685 chip you are setting the value for.
-   * 2) Adjust setOscillatorFrequency() until the PWM update frequency is the
-   *    expected value (50Hz for most ESCs)
-   * Setting the value here is specific to each individual I2C PCA9685 chip and
-   * affects the calculations for the PWM update frequency. 
-   * Failure to correctly set the int.osc value will cause unexpected PWM results
-   */
-  //pwm.setOscillatorFrequency(27000000);
-  pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
-
-  delay(10);
-}
-
-// You can use this function if you'd like to set the pulse length in seconds
-// e.g. setServoPulse(0, 0.001) is a ~1 millisecond pulse width. It's not precise!
-void setServoPulse(uint8_t n, double pulse) {
-  double pulselength;
-  
-  pulselength = 1000000;   // 1,000,000 us per second
-  pulselength /= SERVO_FREQ;   // Analog servos run at ~60 Hz updates
-  Serial.print(pulselength); Serial.println(" us per period"); 
-  pulselength /= 4096;  // 12 bits of resolution
-  Serial.print(pulselength); Serial.println(" us per bit"); 
-  pulse *= 1000000;  // convert input seconds to us
-  pulse /= pulselength;
-  Serial.println(pulse);
-  pwm.setPWM(n, 0, pulse);
-}
-
-void loop() {
-  // Drive each servo one at a time using setPWM()
-  Serial.println(servonum);
-  for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
-    pwm.setPWM(servonum, 0, us2count(pulselen));
-  }
-
-  delay(500);
-  for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
-    pwm.setPWM(servonum, 0, us2count(pulselen));
-  }
-
-  delay(500);
-
-  // Drive each servo one at a time using writeMicroseconds(), it's not precise due to calculation rounding!
-  // The writeMicroseconds() function is used to mimic the Arduino Servo library writeMicroseconds() behavior. 
-  //for (uint16_t microsec = USMIN; microsec < USMAX; microsec++) {
-  //  pwm.writeMicroseconds(servonum, microsec);
-  //}
-
-  //delay(500);
-  //for (uint16_t microsec = USMAX; microsec > USMIN; microsec--) {
-  //  pwm.writeMicroseconds(servonum, microsec);
-  //}
-
-  //delay(500);
-
-  //servonum++;
-  //if (servonum > 7) servonum = 0; // Testing the first 8 servo channels
-}
-#endif
-#if 1
 /*************************************************** 
   This is an example for our Adafruit 16-channel PWM & Servo driver
   Servo test - this will drive 16 servos, one after the other
@@ -139,6 +18,7 @@ void loop() {
 
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
+#include <Adafruit_NeoPixel.h>
 #include <EEPROM.h>
 
 // called this way, it uses the default address 0x40
@@ -439,6 +319,12 @@ int pirState = LOW;
 int runAnimation = false;
 int trigPin = 9;
 int echoPin = 8;
+#define LED_PIN   6
+#define LED_COUNT 4
+
+// Declare our NeoPixel strip object:
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
 float duration_us = 0.0;
 float distance_cm = 0.0;
 float max_distance = 400.0;
@@ -619,6 +505,10 @@ void setup() {
   pinMode(trigPin,OUTPUT);
   pinMode(echoPin,INPUT);
 
+  strip.begin();
+  strip.fill(strip.Color(128,128,128));
+  strip.show(); // Initialize all pixels to 'off'
+
   Serial.begin(115200);
   //while(Serial.read() == -1);
 
@@ -656,11 +546,15 @@ void loop() {
       pirState = HIGH;
       runAnimation = true;
       animationIndex = random(0,5);
+      strip.fill(strip.Color(255,255,255));
+      strip.show(); // Initialize all pixels to 'off'
     }
   } else {
     if (pirState == HIGH){
       pirState = LOW;
       runAnimation = false;
+      strip.fill(strip.Color(128,128,128));
+      strip.show(); // Initialize all pixels to 'off'
       headPanServo.targetPw = headPanServo.centerPw;
       headTiltServo.targetPw = headTiltServo.centerPw;
       leftEyePanServo.targetPw = leftEyePanServo.centerPw;
@@ -957,5 +851,3 @@ void loop() {
   }
   updatePosition();
 }
-
-#endif
